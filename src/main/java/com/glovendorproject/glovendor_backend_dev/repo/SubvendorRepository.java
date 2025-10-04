96699d981d2f0e868bb6b434280d69ba71bb1769
CloudsoftGithub <cloudsoftconsultingltd@gmail.com>
2025-10-04 15:25:38 +0100

    First commit, Glo_Vendor_Reseller_04_October_2025

---
+++ src/main/java/com/glovendorproject/glovendor_backend_dev/repo/SubvendorRepository.java
@@ -0,0 +1,15 @@
+package com.glovendorproject.glovendor_backend_dev.repo;
+import com.glovendorproject.glovendor_backend_dev.entity.Subvendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface SubvendorRepository extends JpaRepository<Subvendor, Long> {
+
+    // Find by unique business name
+    Optional<Subvendor> findByBusinessname(String businessname);
+
+    // Check if a subvendor exists with a given business name
+    boolean existsByBusinessname(String businessname);
+}
---
+++ src/test/java/com/glovendorproject/glovendor_backend_dev/GlovendorBackendDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glovendorproject.glovendor_backend_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovendorBackendDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
